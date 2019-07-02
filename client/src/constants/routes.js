60b0c62d14ad7df4e6771ccd1842eec6dcc54a4e
ajboni <mail@aboni.dev>
2019-07-01 22:10:04 -0300

    added react router

--- client/src/constants/routes.js
+++ client/src/constants/routes.js
@@ -5,31 +5,38 @@ export const routes = [
   {
     target: "/",
     label: "home",
-    icon: "home"
+    icon: "home",
+    component:
+    exact: true
   },
   {
     target: "/race",
     label: c.race,
-    icon: "directions_car"
+    icon: "directions_car",
+    exact: false
   },
   {
     target: "/racers",
     label: c.racers,
-    icon: "group"
+    icon: "group",
+    exact: false
   },
   {
     target: "/ruleset",
     label: c.ruleset,
-    icon: "library_books"
+    icon: "library_books",
+    exact: false
   },
   {
     target: "/stats",
     label: c.stats,
-    icon: "dashboard"
+    icon: "dashboard",
+    exact: false
   },
   {
     target: "/settings",
     label: c.settings,
-    icon: "settings"
+    icon: "settings",
+    exact: false
   }
 ];

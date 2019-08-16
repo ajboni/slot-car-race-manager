e1c3152bf3c6ed6dc0db483e32d34d1e13bfd6e2
ajboni <mail@aboni.dev>
2019-08-16 00:54:07 -0300

    WIP. Race Selection

--- client/src/components/content/race.jsx
+++ client/src/components/content/race.jsx
@@ -1,7 +1,157 @@
-import React from "react";
+import React, { useState, Fragment } from "react";
+import { observer } from "mobx-react";
+import Button from "@material-ui/core/Button";
+import DataTable from "../dataTable";
+import Racer_edit_screen from "./racer_edit";
+import racers_store from "../../stores/racers_store";
+import l from "../../constants/lang";
+import { Query, graphql } from "react-apollo";
+import gql from "graphql-tag";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import store from "../../store";
+import { getAction } from "../dataTable";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/styles";
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  Input,
+  FormHelperText,
+  MenuItem
+} from "@material-ui/core";
 
-function Race() {
-  return <div>Race</div>;
-}
+const GET_RULES = gql`
+  query GetRules {
+    ruleset {
+      id
+      name
+      total_laps
+      total_racers
+    }
+  }
+`;
+
+const GET_RACERS = gql`
+  query GetRacers {
+    racer {
+      id
+      name
+    }
+  }
+`;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(3, 2)
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 400
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2)
+  }
+}));
+
+const Race = observer(() => {
+  const classes = useStyles();
+  const { loading, error, data } = useQuery(GET_RULES);
+  const { loading_racers, error_racers, racers } = useQuery(GET_RACERS);
+
+  const [selectedRuleset, setselectedRuleset] = useState(null);
+  const rulesets = data.ruleset;
+  if (loading | loading_racers) {
+    return <div>Loading...</div>;
+  }
+  if (error | error_racers) {
+    return (
+      <div>
+        Error! {error.message} - {error_racers.message}
+      </div>
+    );
+  }
+
+  if (!!!selectedRuleset) {
+    setselectedRuleset(rulesets[0]);
+  }
+  return (
+    <Paper className={classes.root}>
+      <h1>{l.RACE}</h1>
+
+      <FormControl className={classes.formControl}>
+        <InputLabel>{l.RULESET}</InputLabel>
+        <Select
+          value={selectedRuleset}
+          onChange={e => {
+            setselectedRuleset(e.target.value);
+          }}
+        >
+          {rulesets.map(ruleset => (
+            <MenuItem value={ruleset} key={ruleset.id}>
+              {ruleset.name} ( {ruleset.total_racers} {l.TOTAL_RACERS} |{" "}
+              {ruleset.total_laps} {l.TOTAL_LAPS} )
+            </MenuItem>
+          ))}
+        </Select>
+        {/* <FormHelperText>Some important helper text</FormHelperText> */}
+      </FormControl>
+      <br />
+      <br />
+      <br />
+
+      <Racer_Selector ruleset={selectedRuleset} />
+    </Paper>
+  );
+});
 
 export default Race;
+
+const Racer_Selector = observer(({ ruleset }) => {
+  const classes = useStyles();
+
+  console.log(ruleset);
+  const { loading, error, racers } = useQuery(GET_RACERS);
+  const [selectedRacers, setSelectedRacers] = useState([]);
+
+  let arr = [];
+  for (let index = 0; index < ruleset.total_racers; index++) {
+    arr.push["ss"];
+  }
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error! {error.message}</div>;
+  }
+  return (
+    <Fragment>
+      <div>{ruleset.name}</div>
+      <FormControl className={classes.formControl}>
+        {arr.map(
+          (e, i) => (
+            <div key={i}>hello</div>
+          )
+          // <Fragment>
+          //   <InputLabel>{l.RACERS}</InputLabel>
+          //   <Select
+          //     key={i}
+          //     value={selectedRacers[i]}
+          //     onChange={e => {
+          //       setSelectedRacers(e.target.value);
+          //     }}
+          //   >
+          //     Racer Name
+          //     {/* {rulesets.map(ruleset => (
+          //       <MenuItem value={ruleset} key={ruleset.id}>
+          //         {ruleset.name} ( {ruleset.total_racers} {l.TOTAL_RACERS} |{" "}
+          //         {ruleset.total_laps} {l.TOTAL_LAPS} )
+          //       </MenuItem>
+          //     ))} */}
+          //   </Select>
+          // </Fragment>
+        )}
+      </FormControl>
+    </Fragment>
+  );
+});
